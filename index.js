4ddeda34fb33757478faf0b259cdad3475005ac4
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-06-19 16:58:05 +0200

    e

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.header( 
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
