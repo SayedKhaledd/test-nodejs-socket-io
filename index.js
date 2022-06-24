113f47111ba84bd17bf3f122c5e11a427702c647
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-06-24 03:19:54 +0200

    up

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-//const dbRoute = require("./routes/login_signup_route");
-
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header( 
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.sendFile(__dirname + "/index.html");
 });
-app.listen(process.env.PORT||8080, function () {
-  console.log("Server running");
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("listening on *:3000");
 });
