8491978386ab6ea3245a70f990ecc9cb695f2b7f
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-06-24 03:30:23 +0200

    up

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+  socket.on("chat message", "hi");
 });
 
 server.listen(process.env.PORT || 3000, () => {
